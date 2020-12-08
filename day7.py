f54c0ce32c9c3e01c59265ce48f2be83df6e0bcb
Katherine Turk <35183618+kbturk@users.noreply.github.com>
2020-12-08 12:09:49 -0600

    day 7 part 1 & 2

--- day7.py
+++ day7.py
@@ -15,34 +15,48 @@ def parse_it(line: str):
 
     return key_color, value_description
 
+
+def master_count(key,d):
+    for (_,v) in d[key]:
+        if v =='shiny gold':
+            return 1
+        elif v in d.keys():
+            if master_count(v,d) == 1:
+                return 1
+        else:
+            return 0
+
+def tot_bag_container(key,d):
+    sum = 0
+    #print(f'walking with: {key}, sum = {sum}')
+    if d[key] == []:
+        #print(f'end, {key}: {sum}')
+        pass
+    else:
+        for (num, bag) in d[key]:
+            sum += num + num * tot_bag_container(bag,d)
+            #print(f'current total for {key} last ran {bag} with number {num}: {sum}')
+    return sum
+
 def main(args):
-    #part 1. Not that pretty...
+    #part 1.
     d = {}
     with open(args[1], 'r') as f:
         for line in f:
             rule = parse_it(line)
             d[rule[0]] = rule[1]
-    print(f"{d}")
+    #print(f"{d}")
     tot = 0
     for key in d:
-        print(f'running with {key}, {d[key]}')
+        #print(f'running with {key}, {d[key]}')
         res = master_count(key, d)
         if res == None:
             pass
         else:
             tot += 1
     print(tot)
+    print(tot_bag_container('shiny gold',d))
 
-def master_count(key,d):
-    for (_,v) in d[key]:
-        if v =='shiny gold':
-            print(f'{key} contains: shiny gold. returning a 1')
-            return 1
-        elif v in d.keys():
-            print(f'running master count again. old key was: {key}, new key is: {v},')
-            if master_count(v,d) == 1:
-                return 1
-        else:
-            return 0
+    
 if __name__ == '__main__':
     sys.exit( main( sys.argv ) )
