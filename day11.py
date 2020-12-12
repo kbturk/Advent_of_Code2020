45ff82bbd236759455170f4d36348a7cc1b96937
Katherine Turk <kbturk87@gmail.com>
2020-12-12 11:31:42 -0600

    part 1 solution day 11

--- day11.py
+++ day11.py
@@ -3,51 +3,58 @@ from copy import deepcopy
 
 from typing import Dict, List, Optional, Set, Tuple
 
-def seat_count( row: int, col:int, d:Dict[Tuple[int,int], str] ) -> int:
+def seat_count( row: int, col:int, l:List[List[str]] ) -> int:
     count = 0
     for delta_row in [-1, 0, 1]:
         for delta_col in [-1, 0, 1]:
-            if (row+delta_row, col+delta_col) not in d.keys():
+            #pass if out of bounds.
+            if row+delta_row < 0 or row+delta_row >= len(l):
                 pass
-            elif d[(row+delta_row, col+delta_col)] == '#':
+            elif col+delta_col < 0 or col+delta_col >= len(l[0]):
+                pass
+            #pass if self
+            elif (delta_col,delta_row) == (0,0):
+                pass
+            #otherwise count
+            elif l[row+delta_row][col+delta_col] == '#':
                 count += 1
     return count
 
-def seat_move( WIDTH: int, HEIGHT: int, d:Dict[Tuple[int,int],str]) -> Dict[Tuple[int,int],str]:
-    d_next = deepcopy(d)
-
-    for i in range(WIDTH):
-        for j in range(HEIGHT):
-            if d[(i,j)] == 'L':
-                count = seat_count(i,j,d)
+def seat_move( HEIGHT: int, WIDTH: int, l:List[List[str]] ) -> List[List[str]]:
+    l_next: List[List[str]] = deepcopy(l)
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            if l[i][j] == 'L':
+                count = seat_count(i,j,l)
                 if count == 0:
-                    d_next[(i,j)] = "#"
-            elif d[(i,j)] == "#":
-                count = seat_count(i,j,d)
-                if count >= 3:
-                    d_next[(i,j)] = "L"
-    return d_next
-def main(args: List[str]) -> int:
+                    l_next[i][j] = "#"
+            elif l[i][j] == "#":
+                count = seat_count(i,j,l)
+                if count >= 4:
+                    l_next[i][j] = "L"
+            else:
+                pass
+    return l_next
 
-    d: Dict[Tuple[int,int],str] = {}
-    
+def main(args: List[str]) -> int:
     with open(args[1], 'r') as f:
-        l = [c for line in f for c in line.strip().split()]
+        l: List[List[str]] = [list(line.strip()) for line in f]
+
     HEIGHT, WIDTH = len(l), len(l[0])
 
-    #char_list = [c for line in l for c in line]
-    for i in range(WIDTH):
-        for j in range(HEIGHT):
-            d[(i,j)] = l[i][j]
+    l_next: List[List[str]] = seat_move(HEIGHT,WIDTH,l)
 
-    d = seat_move(WIDTH,HEIGHT,d)
-    print(f'move 1: {d.values()}')
-    d = seat_move(WIDTH,HEIGHT,d)
-    print(f'move 2: {d.values()}')
-    d = seat_move(WIDTH,HEIGHT,d)
-    print(f'move 3: {d.values()}')
-    
+    while l_next != l:
+        l = deepcopy(l_next)
+        l_next = seat_move(HEIGHT,WIDTH,l)
+
+    count = 0
+    for line in l:
+        for c in line:
+            if c == "#":
+                count += 1
+    print(count)
     return 0
-    
+
 if __name__ == '__main__':
     sys.exit( main( sys.argv ) )
