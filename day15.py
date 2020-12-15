a66b2d5fa7d7916e5b39f91b47eb92b6c9e7bc01
Katherine Turk <kbturk87@gmail.com>
2020-12-15 10:14:48 -0600

    day 15 part 2 and a better part 1

--- day15.py
+++ day15.py
@@ -3,33 +3,35 @@ from copy import deepcopy
 
 from typing import Dict, List, Optional, Set, Tuple
 
-         
+
 def main(args: List[str]) -> int:
     registrar: Dict[int,int]= {}
 
     with open(args[1], 'r') as f:
         input = [ int(c) for line in f for c in line.strip().split(',') ]
     print(input)
-    
-    simulation = []
-    
-    for i in range(len(input)):
-        simulation.append(input[i])
 
-    
+    simulation:Dict[int,int] = {}
+
+    for i in range(len(input)):
+        simulation[input[i]] = i
+
+    print(simulation)
+
+    next_entry = 0
+    next_next_entry = 0
 
     for i in range(len(input),30000000):
-        if simulation.count(simulation[-1]) > 1:
-            #print(len(simulation),(simulation.index(simulation[-1])))
-            simulation.append(int(len(simulation) -1 - simulation.index(simulation[-1])))
-            #print(f'simulation after appending: {simulation}')
-            simulation[simulation.index(simulation[-2])] = "X"
-            #print(f'simulation after removal: {simulation}')
+        if next_entry in simulation.keys():
+            next_next_entry = i - simulation[next_entry]
+            simulation[next_entry] = i
+            next_entry = deepcopy(next_next_entry)
         else:
-            simulation.append(0)
-            #print(f'simulation after appending: {simulation}')
-        #print(simulation)
-    print(simulation[-1])
+            simulation[next_entry] = i
+            next_entry = 0
+        if i == 29999998:
+            print(next_entry)
+
     return 0
 
 if __name__ == '__main__':
