969c61d4ba2e45cb9215fcb083815343229d8eba
Katherine Turk <kbturk87@gmail.com>
2020-12-18 23:55:36 -0600

    day18 pt2

--- day18.py
+++ day18.py
@@ -12,27 +12,9 @@ def nat(inp: str) -> Optional[Tuple[str,int]]:
         return None
     return inp, int("".join(digits))
 
-def expr(inp:str) -> Optional[Tuple[str,int]]:
-    res = term(inp)
-    if res is None:
-        return None
-    rest, t = res
-    while True:
-        res = add_term(rest)
-        if res is not None:
-            rest, add_t = res
-            t += add_t
-        else:
-            res = mul_term(rest)
-            if res is not None:
-                rest, mul_t = res
-                t *= mul_t
-            else:
-                break
-    return rest, t
-
 def term(inp:str) -> Optional[Tuple[str,int]]:
     res = nat(inp)
+    #print(f'term res: {res}')
     if res is not None:
         return res
 
@@ -46,15 +28,49 @@ def term(inp:str) -> Optional[Tuple[str,int]]:
         return None
     return inp[1:], ex
 
+def factor(inp:str) -> Optional[Tuple[str,int]]:
+    res = term(inp)
+    #print(f'factor res: {res}')
+    if res is None:
+        return None
+    rest, t = res
+    while True:
+        res = add_term(rest)
+        if res is not None:
+            rest, add_t = res
+            t += add_t
+        else:
+            break
+    #print(f'returning factor res: {rest}')
+    return rest, t
+
 def add_term(inp:str) -> Optional[Tuple[str,int]]:
     if not inp or inp[0] != '+':
         return None
     return term(inp[1:])
 
 def mul_term(inp:str) -> Optional[Tuple[str,int]]:
-    if not inp or inp[0] !='*':
+    if not inp or inp[0] != '*':
         return None
-    return term(inp[1:])
+    #print(f'mult term returning: {inp}')
+    return factor(inp[1:])
+
+
+def expr(inp:str) -> Optional[Tuple[str,int]]:
+    res = factor(inp)
+    #print(f'expr res: {res}')
+    if res is None:
+        return None
+    rest, t = res
+    while True:
+        res = mul_term(rest)
+        #print(f'expr res is now:{res}')
+        if res is not None:
+            rest, mul_t = res
+            t *= mul_t
+        else:
+            break
+    return rest, t
 
 def main() -> int:
     total = 0
