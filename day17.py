1a32bd1f69ead3213985aa56389ab97ab69ce17f
Katherine Turk <kbturk87@gmail.com>
2020-12-18 19:07:40 -0600

    day17 finished program and day18 part 1

--- day17.py
+++ day17.py
@@ -20,14 +20,14 @@ def main(args: List[str]) -> int:
     #build the initial state of the universe:
     universe = set()
     #row,col,dim1:
-    i,j,k = 0,0,0
+    i,j,k,m = 0,0,0,0
 
     #populate the universe!
     for line in input:
         j = 0
         for c in line:
             if c == "#":
-                universe.add((i,j,k))
+                universe.add((i,j,k,m))
             j+=1
         i += 1
 
@@ -47,15 +47,16 @@ def main(args: List[str]) -> int:
             for i in [-1,0,1]:
                 for j in [-1,0,1]:
                     for k in [-1,0,1]:
-                        #add new empty cells to a potential live cell list.
-                        if (cell[0] + i, cell[1] + j, cell[2] + k ) not in universe:
+                        for m in [-1,0,1]:
+                            #add new empty cells to a potential live cell list.
+                            if (cell[0] + i, cell[1] + j, cell[2] + k, cell[3] + m) not in universe:
 
-                            potential_new_neighbors.add((cell[0] + i, cell[1] + j, cell[2] + k))
-                        #ignore itself.
-                        if i == j == k == 0:
-                            pass
-                        elif (cell[0] + i, cell[1] + j, cell[2] + k ) in universe:
-                            live_neighbors += 1
+                                potential_new_neighbors.add((cell[0] + i, cell[1] + j, cell[2] + k, cell[3] + m ))
+                            #ignore itself.
+                            if i == j == k == m == 0:
+                                pass
+                            elif (cell[0] + i, cell[1] + j, cell[2] + k, cell[3] + m) in universe:
+                                live_neighbors += 1
             if (live_neighbors == 2) or (live_neighbors == 3):
                         universe_next_tick.add(cell)
 
@@ -68,12 +69,12 @@ def main(args: List[str]) -> int:
             for i in [-1,0,1]:
                 for j in [-1,0,1]:
                     for k in [-1,0,1]:
-                        #ignore self.
-                        if i == j == k == 0:
-                            pass
-                        #don't count cell.
-                        if (cell[0] + i, cell[1] + j, cell[2] + k ) in universe:
-                            live_neighbors += 1
+                        for m in [-1,0,1]:
+                            #ignore self.
+                            if i == j == k == m == 0:
+                                pass
+                            elif (cell[0] + i, cell[1] + j, cell[2] + k, cell[3] + m ) in universe:
+                                live_neighbors += 1
             if live_neighbors == 3:
                         universe_next_tick.add(cell)
         universe = deepcopy(universe_next_tick)
---
+++ day18.py
@@ -0,0 +1,76 @@
+#Conway game of life!
+
+import sys, itertools, re
+from typing import Optional, Tuple
+
+def nat(inp: str) -> Optional[Tuple[str,int]]:
+    digits = []
+    while inp and inp[0].isdigit():
+        digits.append(inp[0])
+        inp = inp[1:]
+    if not digits:
+        return None
+    return inp, int("".join(digits))
+
+def expr(inp:str) -> Optional[Tuple[str,int]]:
+    res = term(inp)
+    if res is None:
+        return None
+    rest, t = res
+    while True:
+        res = add_term(rest)
+        if res is not None:
+            rest, add_t = res
+            t += add_t
+        else:
+            res = mul_term(rest)
+            if res is not None:
+                rest, mul_t = res
+                t *= mul_t
+            else:
+                break
+    return rest, t
+
+def term(inp:str) -> Optional[Tuple[str,int]]:
+    res = nat(inp)
+    if res is not None:
+        return res
+
+    if not inp or inp[0] != '(':
+        return None
+    res = expr( inp[1:] )
+    if res is None:
+        return None
+    inp, ex = res
+    if not inp or inp[0] !=')':
+        return None
+    return inp[1:], ex
+
+def add_term(inp:str) -> Optional[Tuple[str,int]]:
+    if not inp or inp[0] != '+':
+        return None
+    return term(inp[1:])
+
+def mul_term(inp:str) -> Optional[Tuple[str,int]]:
+    if not inp or inp[0] !='*':
+        return None
+    return term(inp[1:])
+
+def main() -> int:
+    total = 0
+    for line in sys.stdin:
+        stripped = ''.join(c for c in line if not c.isspace() )
+        #print(stripped)
+        res = expr(stripped)
+        if res is None:
+            print( f'Bad expression: {stripped}', file=sys.stderr )
+            return 1
+        rest, val = res
+        if rest:
+            print( f'extra input: {rest}', file=sys.stderr )
+        total += val
+    print(total)  
+    return 0
+
+if __name__ == '__main__':
+    sys.exit( main( ) )
